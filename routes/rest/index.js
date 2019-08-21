3be73db7885ca4053000051f253555c2a1d8a7b2
Indrajit Roy <indrajit@logic-square.com>
2019-08-21 16:50:14 +0530

    Initial commit

---
+++ routes/rest/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const router = express.Router()
+
+const expressJwt = require("express-jwt")
+
+const config = require("../../config")[process.env.NODE_ENV || "development"]
+
+const checkJwt = expressJwt({ secret: config.secret }) // the JWT auth check middleware
+
+const login = require("./auth")
+const signup = require("./auth/signup")
+const forgotpassword = require("./auth/password")
+const users = require("./users")
+
+router.post("/login", login.post) // UNAUTHENTICATED
+router.post("/signup", signup.post) // UNAUTHENTICATED
+router.post("/forgotpassword", forgotpassword.startWorkflow) // UNAUTHENTICATED; AJAX
+router.post("/resetpassword", forgotpassword.resetPassword) // UNAUTHENTICATED; AJAX
+
+router.all("*", checkJwt) // use this auth middleware for ALL subsequent routes
+
+router.get("/users", users.find)
+router.get("/user/:id", users.get)
+router.post("/user", users.post)
+router.put("/user/:id", users.put)
+router.delete("/user/:id", users.delete)
+
+module.exports = router
